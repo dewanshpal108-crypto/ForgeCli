f094a1e5454b01668fa2ea72dab56876a9497c11
Prashant Pal <prashantpal2468@gmail.com>
2026-07-07 19:40:55 +0530

    approval flow

--- modes/agent/approval.ts
+++ modes/agent/approval.ts
@@ -1,5 +1,5 @@
 import { ActionTracker } from "./action-tracker";
-
+//this is approval flow for the agent, it will be used to approve or reject actions that require user approval
 export async function runApprovalFlow(tracker: ActionTracker): Promise<boolean> {
     return true;
 }
